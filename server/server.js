dc75add46e6e58d6f6ecb41439203ff7b461b684
Szymon Tyburek <szymontyburek10@gmail.com>
2024-06-06 15:06:16 -0700

    call is made to openai api when port 8080 is visited (if server is running)

--- server/server.js
+++ server/server.js
@@ -1,19 +1,30 @@
 require("dotenv").config();
 const OpenAI = require("openai");
+const app = require("express")();
+const port = 8080;
+
+app.listen(port, () => {
+  console.log("Server is running on port " + port);
+});
+
+app.get("/", async (req, res) => {
+  res.json({ message: await callToOpenAi() });
+});
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-async function main() {
+async function callToOpenAi() {
+  let response;
   const chatCompletion = await openai.chat.completions
     .create({
-      messages: [{ role: "user", content: "Say this is a test" }],
+      messages: [{ role: "user", content: "Say this is a dfst" }],
       model: "gpt-3.5-turbo",
     })
     .then((res) => {
-      console.log(res.choices[0].message);
+      response = res.choices[0].message.content;
     });
-}
 
-main();
+  return response;
+}
