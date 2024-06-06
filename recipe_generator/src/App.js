d208adc4d268d038bb4ce5cd695b6f1d7d4448a1
Szymon Tyburek <szymontyburek10@gmail.com>
2024-06-06 15:33:19 -0700

    client is displaying TextField and Submit React components

--- recipe_generator/src/App.js
+++ recipe_generator/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Recipe Generator</h1>
+      <TextField />
+      <Submit />
     </div>
   );
 }
+
+function TextField() {
+  return <textarea></textarea>;
+}
+
+function Submit() {
+  return <button>Submit</button>;
+}
+
+function getMessage() {}
 
 export default App;
