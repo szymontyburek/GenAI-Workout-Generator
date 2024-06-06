cf7de580a621f461630dbed781ef6d06f72b5d9d
Szymon Tyburek <szymontyburek10@gmail.com>
2024-06-06 12:20:59 -0700

    I setup an endpoint on port 8080. It can be seen by running node on server.js file

--- server/server.js
+++ server/server.js
@@ -4,3 +4,11 @@ const OpenAI = require("openai");
 
 const app = express();
 const PORT = 8080;
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello World" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
