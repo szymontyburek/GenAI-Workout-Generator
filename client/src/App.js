78341e25970bfdaa7e21c9ea2cf98d0a3b101448
Szymon Tyburek <szymontyburek10@gmail.com>
2024-06-14 10:37:30 -0700

    refacotred HTML/CSS for easier responsiveness implementation later on

--- client/src/App.js
+++ client/src/App.js
@@ -2,55 +2,34 @@ import "./App.css";
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-function TextField({ text, onType, placeholder }) {
-  const [nodeValue, setNodeValue] = useState("");
-  const [Placeholder, setPlaceholder] = useState("");
+function ParentContainer() {
+  const [switchToAnimation, setSwitchToAnimation] = useState(false);
+  const [styles, setStyles] = useState({
+    width: "100%",
+    height: "100%",
+    position: "absolute",
+    top: 0,
+    left: 0,
+    backgroundColor: "black",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  });
 
   useEffect(() => {
-    setNodeValue(text);
-  }, [text]);
+    //modify display property for topmost div in LoadingAnimation component dynamically
+    let clone = structuredClone(styles);
 
-  useEffect(() => {
-    setPlaceholder(placeholder);
-  }, [placeholder]);
+    if (switchToAnimation) clone.display = "flex";
+    else clone.display = "none";
 
-  function getText(e) {
-    const val = e.target.value;
-    setNodeValue(val);
-    onType(val); //pass data to parent
-  }
+    setStyles(clone);
+  }, [switchToAnimation]);
 
   return (
-    <textarea
-      style={{ width: "80%", height: "5em", margin: "1em 0" }}
-      value={nodeValue}
-      onChange={getText}
-      placeholder={Placeholder}
-    ></textarea>
-  );
-}
-
-function Submit({ data, onClick }) {
-  return (
-    <button
-      style={{ padding: "1em", width: "80%" }}
-      onClick={() => onClick(data)}
-    >
-      Submit
-    </button>
-  );
-}
-
-function ImgContainer({ src, onResponse }) {
-  const [imgSrc, setImgSrc] = useState("");
-
-  useEffect(() => {
-    setImgSrc(src);
-  }, [src]);
-
-  return (
-    <div style={{ width: "80%", height: "50%", border: "1px solid black" }}>
-      <img style={{ width: "100%", height: "100%" }} src={src}></img>
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <ImageGeneration setSwitchToAnimation={setSwitchToAnimation} />
+      <LoadingAnimation style={styles} />
     </div>
   );
 }
@@ -92,7 +71,7 @@ function ImageGeneration({ setSwitchToAnimation }) {
         flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
-        width: "100vw",
+        width: "80vw",
         height: "100vh",
       }}
     >
@@ -102,8 +81,17 @@ function ImageGeneration({ setSwitchToAnimation }) {
         text={sharedUserMessage}
         onType={setSharedUserMessage}
         placeholder={sharedPlaceholder}
-      />{" "}
-      <Submit data={sharedUserMessage} onClick={exportText} />
+      />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          width: "inherit",
+        }}
+      >
+        <Button text="Download" data={sharedUserMessage} onClick={exportText} />
+        <Button text="Submit" data={sharedUserMessage} onClick={exportText} />
+      </div>
     </div>
   );
 }
@@ -112,39 +100,57 @@ function LoadingAnimation({ style }) {
   return (
     <div style={style}>
       <div className="loadingWheel"></div>
-      <ImgContainer src="patience.jpg" style={{ width: "80%" }} />
+      <ImgContainer src="patience.jpg" />
     </div>
   );
 }
 
-function ParentContainer() {
-  const [switchToAnimation, setSwitchToAnimation] = useState(false);
-  const [styles, setStyles] = useState({
-    width: "100%",
-    height: "100%",
-    position: "absolute",
-    top: 0,
-    left: 0,
-    backgroundColor: "black",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-  });
+function TextField({ text, onType, placeholder }) {
+  const [nodeValue, setNodeValue] = useState("");
+  const [Placeholder, setPlaceholder] = useState("");
 
   useEffect(() => {
-    //modify display property for topmost div in LoadingAnimation component dynamically
-    let clone = structuredClone(styles);
+    setNodeValue(text);
+  }, [text]);
 
-    if (switchToAnimation) clone.display = "flex";
-    else clone.display = "none";
+  useEffect(() => {
+    setPlaceholder(placeholder);
+  }, [placeholder]);
 
-    setStyles(clone);
-  }, [switchToAnimation]);
+  function getText(e) {
+    const val = e.target.value;
+    setNodeValue(val);
+    onType(val); //pass data to parent
+  }
 
   return (
-    <div>
-      <ImageGeneration setSwitchToAnimation={setSwitchToAnimation} />
-      <LoadingAnimation style={styles} />
+    <textarea
+      style={{ width: "inherit", height: "5em", margin: "1em 0" }}
+      value={nodeValue}
+      onChange={getText}
+      placeholder={Placeholder}
+    ></textarea>
+  );
+}
+
+function Button({ text, data, onClick }) {
+  return (
+    <button style={{ padding: "1em" }} onClick={() => onClick(data)}>
+      {text}
+    </button>
+  );
+}
+
+function ImgContainer({ src, onResponse }) {
+  const [imgSrc, setImgSrc] = useState("");
+
+  useEffect(() => {
+    setImgSrc(src);
+  }, [src]);
+
+  return (
+    <div style={{ width: "inherit", height: "50%", border: "1px solid black" }}>
+      <img style={{ width: "100%", height: "100%" }} src={src}></img>
     </div>
   );
 }
