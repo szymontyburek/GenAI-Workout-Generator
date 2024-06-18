dd8c4d804d609b0765587c0ee3566d0647f65a99
Szymon Tyburek <szymontyburek10@gmail.com>
2024-06-18 11:21:50 -0700

    Open Modal button is a React Component

--- client/src/modal.js
+++ client/src/modal.js
@@ -35,6 +35,58 @@ function Modal() {
     modal.classList.remove("active");
     overlay.classList.remove("active");
   }
+
+  return (
+    // <div class="modal" id="modal">
+    //   <div class="modal-header">
+    //     <button data-close-button class="close-button">&times;</button>
+    //     <div
+    //       style="display: flex; justify-content: center; align-items: center"
+    //     >
+    //       <div class="title">Prior Generations:</div>
+    //       <select name="selectDate" id="">
+    //         <option value="">06/17/2024</option>
+    //         <option value="">06/16/2024</option>
+    //       </select>
+    //     </div>
+    //   </div>
+    //   <div class="modal-body">
+    //     <img src="logo512.png" alt="" />
+    //     <img src="logo512.png" alt="" />
+    //     <img src="logo512.png" alt="" />
+    //     <img src="logo512.png" alt="" />
+    //     <img src="logo512.png" alt="" />
+    //     <div
+    //       style="
+    //         display: flex;
+    //         justify-content: space-around;
+    //         align-items: center;
+    //       "
+    //     >
+    //       <button>Cancel</button>
+    //       <button style="background-color: #ee2400">Download</button>
+    //     </div>
+    //   </div>
+    // </div>
+    // <div id="overlay"></div>
+    <Button1 />
+  );
+}
+
+function Button1() {
+  return <button data-modal-target="#modal">Open Modal</button>;
+}
+
+function Button2() {
+  return (
+    <button data-close-button class="close-button">
+      &times;
+    </button>
+  );
+}
+
+function ModalContainer() {
+  return <div className="modal" id="modal"></div>;
 }
 
 export default Modal;
