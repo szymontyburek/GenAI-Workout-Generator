6cf9937395bd01e1aa045bdfbc7588f72a29ef09
Szymon Tyburek <szymontyburek10@gmail.com>
2024-06-18 11:27:58 -0700

    modal is a React component

--- client/src/modal.js
+++ client/src/modal.js
@@ -1,41 +1,4 @@
 function Modal() {
-  const openModalButtons = document.querySelectorAll("[data-modal-target]");
-  const closeModalButtons = document.querySelectorAll("[data-close-button]");
-  const overlay = document.getElementById("overlay");
-
-  openModalButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-      const modal = document.querySelector(button.dataset.modalTarget);
-      openModal(modal);
-    });
-  });
-
-  overlay.addEventListener("click", () => {
-    const modals = document.querySelectorAll(".modal.active");
-    modals.forEach((modal) => {
-      closeModal(modal);
-    });
-  });
-
-  closeModalButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-      const modal = button.closest(".modal");
-      closeModal(modal);
-    });
-  });
-
-  function openModal(modal) {
-    if (modal == null) return;
-    modal.classList.add("active");
-    overlay.classList.add("active");
-  }
-
-  function closeModal(modal) {
-    if (modal == null) return;
-    modal.classList.remove("active");
-    overlay.classList.remove("active");
-  }
-
   return (
     // <div class="modal" id="modal">
     //   <div class="modal-header">
@@ -69,7 +32,10 @@ function Modal() {
     //   </div>
     // </div>
     // <div id="overlay"></div>
-    <Button1 />
+    <div>
+      <Button1 />
+      <ModalContainer />
+    </div>
   );
 }
 
