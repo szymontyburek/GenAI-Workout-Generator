e7cc5cc20130c4d4ed5a58db8be5b10b17967d1c
Szymon Tyburek <szymontyburek10@gmail.com>
2024-06-22 11:52:07 -0700

    refactored db.js to connect, read from db, then disconnect each time a call is made to the db

--- server/db.js
+++ server/db.js
@@ -1,32 +1,6 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri = process.env.NEW_CONNECTION;
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
 
 module.exports = {
   userModel: mongoose.model(
@@ -36,7 +10,26 @@ module.exports = {
       description: String,
     })
   ),
-  connectToDb: mongoose.connect(process.env.MONGO_CONNECTION).then(() => {
-    console.log("Successfully connected to database");
-  }),
+
+  getData: async function () {
+    const connection = new MongoClient(process.env.MONGODB_CONNECTION, {
+      serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+      },
+    });
+
+    let data;
+    try {
+      await connection.connect();
+      const collection = connection.db("ImageGenerator").collection("images");
+      data = await collection.find().toArray();
+    } catch (err) {
+      console.log(err);
+    } finally {
+      await connection.close();
+    }
+    return data;
+  },
 };
