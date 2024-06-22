dc319e940b70176f9466b34430e8998cf08dd0f4
Szymon Tyburek <szymontyburek10@gmail.com>
2024-06-22 16:12:22 -0700

    refactored code in db.js for easier maintainability in the future

--- server/db.js
+++ server/db.js
@@ -2,26 +2,29 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
-const log = function () {
-  console.log("lkj");
-};
-
-const getRecords = async function () {
-  log();
-  const connection = new MongoClient(process.env.MONGODB_CONNECTION, {
+const getConnection = async function () {
+  return new MongoClient(process.env.MONGODB_CONNECTION, {
     serverApi: {
       version: ServerApiVersion.v1,
       strict: true,
       deprecationErrors: true,
     },
   });
+};
 
+const getCollection = async function (connection) {
+  await connection.connect();
+  return connection.db("ImageGenerator").collection("images");
+};
+
+const getRecords = async function () {
   let data;
   let success;
+  let connection;
 
   try {
-    await connection.connect();
-    const collection = connection.db("ImageGenerator").collection("images");
+    connection = await getConnection();
+    const collection = await getCollection(connection);
     data = await collection.find().toArray();
     success = true;
   } catch (err) {
@@ -34,20 +37,13 @@ const getRecords = async function () {
 };
 
 const addRecord = async function (fieldDataObj) {
-  const connection = new MongoClient(process.env.MONGODB_CONNECTION, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    },
-  });
-
   let data;
   let success;
+  let connection;
 
   try {
-    await connection.connect();
-    const collection = connection.db("ImageGenerator").collection("images");
+    connection = await getConnection();
+    const collection = await getCollection(connection);
     const result = await collection.insertOne(fieldDataObj);
     success = true;
   } catch (err) {
